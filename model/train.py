ea665a97ed637736235a10f49add3c27949998be
David Dubach <david.dubach96@gmail.com>
2020-12-05 16:40:41 +0100

    notes on how to adjust model.py and train.py

--- model/train.py
+++ model/train.py
@@ -1,3 +1,12 @@
+"""
+ToDo (David):
+- use Wrapper class to load devie (CPU, GPU) and model
+- use Dataset class to load training data
+- follow tutorial "main()" implementation
+- i think we need to remove the call to "evaluate" (alex said that in the tutorial session, should be written somewhere)
+"""
+
+
 def main():
     # TODO train loop here!
     # TODO don't forget to save the model's weights inside of `./weights`!
