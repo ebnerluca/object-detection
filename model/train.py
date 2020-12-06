594b86e65ac1abae3d650d0324cd320b2333fc50
David Dubach <david.dubach96@gmail.com>
2020-12-06 21:14:28 +0100

    get_model adjustment, save parameters after training

--- model/train.py
+++ model/train.py
@@ -1,5 +1,7 @@
 import os
 import torch
+import torchvision
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
 
 import utils
 from model import Model
@@ -13,6 +15,13 @@ ToDo (David):
 - use Dataset class to load training data
 - follow tutorial "main()" implementation
 """
+
+
+def get_model():
+    model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
+    in_features = model.roi_heads.box_predictor.cls_score.in_features
+    model.roi_heads.box_predictor = FastRCNNPredictor(in_features, 5)  # 5 is the number of classes
+    return model
 
 
 def get_transform(train):
@@ -44,7 +53,7 @@ def main():
                                                    num_workers=4, collate_fn=utils.collate_fn)
 
     device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-    model = Model()
+    model = get_model()
     model.to(device)
 
     params = [p for p in model.parameters() if p.requires_grad]
@@ -57,9 +66,12 @@ def main():
     for epoch in range(num_epochs):
         # train for one epoch, printing every 10 iterations
         train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq=10)
+        print("trained one epoch")
         # update the learning rate
         lr_scheduler.step()
 
+    print("Saving weights...")
+    torch.save(model.state_dict(), "weights/model.pt")
     print("That's it!")
 
 
