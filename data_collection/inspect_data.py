b3f558dd9c9aeede70bb17d2f8607291eb1bfdec
Luca Ebner <ebnerl@student.ethz.ch>
2020-12-06 11:21:00 +0100

    Added different box colors for inspect_data.py

--- data_collection/inspect_data.py
+++ data_collection/inspect_data.py
@@ -5,6 +5,16 @@ dataset_folder = "./data_collection/dataset/"  # assuming you run from within re
 min_idx = 0
 max_idx = 10
 
+def get_color(class_idx):
+
+    color = {
+        0: (0,0,0),  # background
+        1: (0,255,0),  # duckie
+        2: (100,100,255),  # cone
+        3: (255,150,50),  # truck
+        4: (255,50,150)  # bus
+    }
+    return color.get(class_idx, (255,255,255))
 
 def inspect_image_and_boxes(npzfile):
 
@@ -22,8 +32,9 @@ def inspect_image_and_boxes(npzfile):
 
     # show img with boxes
     img_boxed = img_bgr.copy()
-    for box in boxes:
-        img_boxed = cv2.rectangle(img_boxed, tuple(box[0:2]), tuple(box[2:4]), (0, 255, 0), 2)
+
+    for i in range(len(boxes)):
+        img_boxed = cv2.rectangle(img_boxed, tuple(boxes[i][0:2]), tuple(boxes[i][2:4]), get_color(classes[i]), 2)
 
     cv2.imshow("boxed_image", img_boxed)
     cv2.waitKey(0)
